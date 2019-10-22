edd370eed07707722510653c0ecc9122edd426b0
schirinturko <schirin.turko24@gmail.com>
2019-10-22 14:40:16 +0200

    Kunde.java - Constr., Getter, Setter, toString hinzugefügt

--- Baeckerei/src/main/java/at/htl/baeckerei/model/Kunde.java
+++ Baeckerei/src/main/java/at/htl/baeckerei/model/Kunde.java
@@ -1,4 +1,40 @@
 package at.htl.baeckerei.model;
 
+@XmlRootElement
 public class Kunde {
+    private int id;
+    private String name;
+
+    //Konstruktors
+    public Kunde() {
+    }
+
+    public Kunde(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //getter
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //setter
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return id + " - " + name;
+    }
 }
--- Baeckerei/src/main/java/at/htl/baeckerei/rest/BaeckereiEndpoint.java
+++ Baeckerei/src/main/java/at/htl/baeckerei/rest/BaeckereiEndpoint.java
@@ -1,8 +1,12 @@
 package at.htl.baeckerei.rest;
 
+import at.htl.baeckerei.model.Kunde;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("baeckerei")
 public class BaeckereiEndpoint {
@@ -10,6 +14,13 @@ public class BaeckereiEndpoint {
     @GET
     @Path("{id}")
     public Kunde find(@PathParam("id") long id){
-        return new Kunde("Lisa " + id);
+        return new Kunde("Lisa " + id, );
+    }
+
+    @GET
+    public List<Kunde> findAll(){
+        List<Kunde> all = new ArrayList<>();
+        all.add(find(42));
+        return all;
     }
 }

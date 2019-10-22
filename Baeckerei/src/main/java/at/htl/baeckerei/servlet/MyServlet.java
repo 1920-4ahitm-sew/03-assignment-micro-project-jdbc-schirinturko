e16aa2a3387a2e7a4b32fad607c16eae31b89cda
schirinturko <schirin.turko24@gmail.com>
2019-10-23 00:30:35 +0200

    MyServlet.java - protected void doGet hinzugefügt

--- Baeckerei/src/main/java/at/htl/baeckerei/servlet/MyServlet.java
+++ Baeckerei/src/main/java/at/htl/baeckerei/servlet/MyServlet.java
@@ -1,4 +1,18 @@
 package at.htl.baeckerei.servlet;
 
-public class MyServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("my")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException{
+        String name = req.getParameter("name");
+        resp.getWriter().printf("<h1>Hello %s</h1>", name);
+    }
 }

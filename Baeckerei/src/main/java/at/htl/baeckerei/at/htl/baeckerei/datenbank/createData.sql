4955af40cb725c452a89eb3f8bfa13024410c028
schirinturko <schirin.turko24@gmail.com>
2019-10-23 11:44:12 +0200

    createDate.sql - table mit inserts erstellt

---
+++ Baeckerei/src/main/java/at/htl/baeckerei/at/htl/baeckerei/datenbank/createData.sql
@@ -0,0 +1,11 @@
+drop table kunde;
+
+create table kunde(
+    id int,
+    name varchar(20)
+);
+
+insert into kunde (id,name) values(19,'Lisa');
+insert into kunde (id,name) values(38,'Stefan');
+
+select * from kunde;
